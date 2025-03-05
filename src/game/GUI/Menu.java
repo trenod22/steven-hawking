abce7ca296f7ae2f41ffe09287671245c6af3fcf
trenod22 <trenod22@htl-kaindorf.at>
2025-03-05 10:49:07 +0100

    Better Menu

--- src/game/GUI/Menu.java
+++ src/game/GUI/Menu.java
@@ -21,15 +21,15 @@ public class Menu {
         g.setFont(new Font("Arial", Font.BOLD, 30));
         String startMessage = "Press Space to Start";
         int textWidth = g.getFontMetrics().stringWidth(startMessage);
-        g.drawString(startMessage, (800 - textWidth) / 2, 400);
+        g.drawString(startMessage, (800 - textWidth) / 2, 600);
 
         String steuerungMessage = "Press Space to Jump";
         int textWidth1 = g.getFontMetrics().stringWidth(steuerungMessage);
-        g.drawString(steuerungMessage, (800 - textWidth1) / 2, 500);
+        g.drawString(steuerungMessage, (800 - textWidth1) / 2, 400);
 
         String steuerung1Message = "Press Mouse to use Jetpack";
         int textWidth2 = g.getFontMetrics().stringWidth(steuerung1Message);
-        g.drawString(steuerung1Message, (800 - textWidth2) / 2, 600);
+        g.drawString(steuerung1Message, (800 - textWidth2) / 2, 500);
 
 
 
